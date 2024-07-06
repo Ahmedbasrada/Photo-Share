05396fe7f23e172a1f0823a4fb4d208cbb954068
AhmedBasrada <Ahmed.basrada@gmail.com>
2024-07-07 00:42:20 +0300

    vercel cors 5

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 app.use(cors({
-  origin: "*",
+  origin: 'https://photo-share-five.vercel.app'
 }));
 
 
