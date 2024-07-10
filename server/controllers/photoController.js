f51045b08ad2945a7a5499f072842d7eef888529
AhmedBasrada <Ahmed.basrada@gmail.com>
2024-07-10 20:56:31 +0300

    tem saving img 6

--- server/controllers/photoController.js
+++ server/controllers/photoController.js
@@ -137,7 +137,7 @@ exports.delete = async(req,res)=>{
             await cloudinary.uploader.destroy(image.public_id);
         } catch (err) {
         
-        return res.status(500).json({massage: image.public_id})
+        return res.status(500).json({massage: err})
         }
         //   حذف بيانات الصورة
         await image.deleteOne()
